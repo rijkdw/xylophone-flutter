cc764f301a29a1d08988d0113817a9378de1b4c2
Rijk-linux <20376316@sun.ac.za>
2020-06-26 12:54:27 +0200

    7 buttons for 7 sounds

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,24 +4,48 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final List<int> numbers = [1, 2, 3, 4, 5, 6, 7];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text(
-                'Click Me',
-              ),
-            ),
+          child: Column(
+            children: numbers.map((i) {
+              return SoundButton(
+                number: i,
+              );
+            }).toList(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class SoundButton extends StatelessWidget {
+  final int number;
+  final List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple,
+  ];
+
+  SoundButton({this.number});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      color: colors[this.number - 1],
+      onPressed: () {
+        final player = AudioCache();
+        player.play('note${this.number}.wav');
+      },
     );
   }
 }
